324e78f6d7e458c60646b4e00777ccf2cc042303
Dona Anda <ceka_dona@yahoo.com>
2018-05-18 13:04:51 -0700

    Init commit

---
+++ src/reducers/user_reducer.js
@@ -0,0 +1,10 @@
+import type from '../actions/types';
+
+const DEFAULT_STATE = {};
+
+export default (state = DEFAULT_STATE, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
