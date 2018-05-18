4b695f6174b5cd2b8c5f0735c5d8000baf3880dc
Dona Anda <ceka_dona@yahoo.com>
2018-05-18 15:32:56 -0700

    Completed basic pages and navbar

---
+++ src/components/secret_doc.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Top Secret Document</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+)
