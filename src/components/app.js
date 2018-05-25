d1e220cc94d1b954572f1454d221ed69755164d7
Dona Anda <ceka_dona@yahoo.com>
2018-05-24 17:14:23 -0700

    completed auth-with-api

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,10 @@ import SecretDoc from './secret_doc';
 import OperativeList from './operative_list';
 import {Route} from 'react-router-dom';
 import auth from '../hoc/auth';
+import SignUp from './sign_up';
+import SignIn from './sign_in';
+import MovieQuote from './movie_quote';
+import redirect from '../hoc/redirect';
 
 const App = () => (
     <div>
@@ -16,6 +20,9 @@ const App = () => (
             <Route path="/about" component={About}/>
             <Route path="/secret-doc" component={auth(SecretDoc)}/>
             <Route path="/operative-list" component={OperativeList}/>
+            <Route path="/sign-up" component={redirect(SignUp, '/movie-quote')}/>
+            <Route path="/sign-in" component={redirect(SignIn, '/movie-quote')}/>
+            <Route path="/movie-quote" component={auth(MovieQuote)}/>
         </div>
     </div>
 );
